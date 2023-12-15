e7ceebade0bb15aca76f722d6750a28860e972c8
mararo-ops <danielmararo1@gmail.com>
2023-12-15 14:15:57 +0300

    starting database engine

--- lib/database.py
+++ lib/database.py
@@ -1,3 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
+
+DATABASE_URI = 'sqlite:///art_gallery.db'
+engine = create_engine(DATABASE_URI)
+
+Session = sessionmaker(bind=engine)
+session = Session()

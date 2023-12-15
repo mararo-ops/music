9ba0335674f688dbdd9ae7acfeb41980201e3c19
mararo-ops <danielmararo1@gmail.com>
2023-12-15 14:26:40 +0300

    adding artwork

--- lib/cli.py
+++ lib/cli.py
@@ -12,3 +12,21 @@ def add_artist():
     session.add(new_artist)
     session.commit()
     print(f"\nAdded artist: {name}")
+
+def add_artwork():
+    print("\nEnter artwork details:")
+    artist_name = input("Artist's Name: ").strip().lower()
+    year_of_making = int(input("Year of Making: "))
+    unique_title = input("Unique Title: ")
+    style_of_art = input("Style of Art: ")
+    price = int(input("Price: "))
+
+    artist = session.query(Artist).filter(Artist.name.ilike(artist_name)).first()
+    if artist:
+        new_artwork = Artwork(artist_id=artist.id, year_of_making=year_of_making, unique_title=unique_title,
+                              style_of_art=style_of_art, price=price)
+        session.add(new_artwork)
+        session.commit()
+        print(f"\nAdded artwork: {unique_title} by {artist_name}")
+    else:
+        print(f"\nArtist {artist_name} not found.")

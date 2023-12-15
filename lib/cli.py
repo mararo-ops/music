eb1653b5f2689f787afbb0e03f1749a33302dd80
mararo-ops <danielmararo1@gmail.com>
2023-12-15 14:26:05 +0300

    adding an artist

--- lib/cli.py
+++ lib/cli.py
@@ -1,2 +1,14 @@
 from models import Artist, Artwork
 from database import session, create_database  
+
+def add_artist():
+    print("\nEnter artist details:")
+    name = input("Name: ")
+    age = int(input("Age: "))
+    birthplace = input("Birthplace: ")
+    style_of_work = input("Style of Work: ")
+
+    new_artist = Artist(name=name, age=age, birthplace=birthplace, style_of_work=style_of_work)
+    session.add(new_artist)
+    session.commit()
+    print(f"\nAdded artist: {name}")

434b08c76bae44c8fa333e8d2be95ee6ef4bad39
mararo-ops <danielmararo1@gmail.com>
2023-12-15 14:18:35 +0300

    declerative base

--- lib/models.py
+++ lib/models.py
@@ -1,3 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+
+Base = declarative_base()

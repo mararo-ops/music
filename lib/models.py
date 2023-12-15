d0859e2c215da39e452d76fb946898ceda3d8833
mararo-ops <danielmararo1@gmail.com>
2023-12-15 14:19:13 +0300

    adding the artist class

--- lib/models.py
+++ lib/models.py
@@ -3,3 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
+
+class Artist(Base):
+    __tablename__ = 'artists'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    birthplace = Column(String)
+    style_of_work = Column(String)
+    artworks = relationship('Artwork', back_populates='artist')
